87b57f1bd8d22739cf71d1f0eca585b05a9e2d76
GinKenta <ginkenta@gmail.com>
2020-03-03 16:50:43 +0200

    feat: solution is added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length % 2) return false;
+  let string = str;
+  for(let i = 0; i < str.length/2; i++){
+    for(let j = 0; j < bracketsConfig.length; j++){
+      const par = bracketsConfig[j].join('');
+      if(string.includes(par)){
+        string = string.replace(par,'');
+      }
+    }
+  }
+  return !string.length;
 }
